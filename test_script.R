fe88568a35c6aa3e5c30dc314ccd469f40e148da
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2021-02-06 22:04:24 +0000

    Create simple 1-unit xy-resolution elev1 raster to understand how cansee() and rasterprofile() operate. viewTo() still gives an error.

--- test_script.R
+++ test_script.R
@@ -1,16 +1,36 @@
 # Test it out
 
-library(spData)
-data(elev)
+library(raster)
 
-plot(elev)
-rcl = matrix(c(28, 36, 20), ncol = 3, byrow = TRUE)
-recl = reclassify(elev, rcl = rcl)
-plot(recl)
+# Elevation raster, 1 m resolution
+elev1 = raster(nrows = 6, ncols = 6, res = 1, 
+                       xmn = 0, xmx = 6, ymn = 0, ymx = 6,
+                       vals = 1:36)
+plot(elev1)
 
-coordinates(recl)
-
-#viewTo(r = recl, xy = matrix(coordinates(recl)), xy2 = matrix(coordinates(recl)))
 
 # rasterprofile samples a raster along a straight line between two points
-rasterprofile(recl, c(37,38), c(1,1))
+# first pair is the coords in raster space (not row,col)
+# 2nd pair ditto
+# each pair of items is x-y coords at start and end
+# e.g. track along top row of map horizontally
+rasterprofile(elev1, c(0.5,5.5), c(5.5,5.5))
+
+# profile along southern most row of map east to west
+rasterprofile(elev1, c(0.5,0.5), c(5.5,0.5))
+
+# profile along western most edge of map, south to north
+# the first value is at the south (bottom left) and the
+# last value north (top left). One elevation is repeated
+# as it uses map units rather than rows and columns
+rasterprofile(elev1, c(0.5,0.5), c(0.5,5.5))
+
+
+# Diagonally from north west to south east
+rasterprofile(elev1, c(0.5,5.5), c(5.5,0.5))
+
+# So what does cansee() return: TRUE or FALSE
+cansee(elev1, c(0.5,0.5), c(5.5,5.5), h1=2, h2=0)
+
+# Does viewTo now work? No. Gives an error.
+viewTo(elev1, c(0.5,0.5), c(5.5,5.5), h1=2, h2=0)
