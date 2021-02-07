c882926be1dceef7f0b104597b6f3350c539c347
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2021-02-07 20:58:49 +0000

    Seems to work successfully in creating multi_point Viewshed. Need to double-check beyond 5km, and create raster output too.

--- cumbria_test.R
+++ cumbria_test.R
@@ -3,13 +3,15 @@
 # worried about slow performance
 
 library(raster)
+library(sf)
 source("initial_LOS.R")
 
 elev500 <- raster("elevation500m.tif")
+view500 <- raster("Viewshed500.tif")
 res(elev500)
-plot(elev500)
 
 mast_coords <- read.csv("mast_coords.csv")
+
 
 # Let's try and return output from viewTo as a points map
 mastx <- as.numeric(mast_coords[1])
@@ -19,16 +21,35 @@ h2 <- 75
 
 xy1_pt <- st_point(c(mastx, masty))
 plot(elev500)
+
+
+# Let us work on a small area to begin with
+# Create a mask and subset main elevation
+rmask = raster(nrows = 50, ncols = 50, res = 500, 
+               xmn = 335000, xmx = 360000, ymn = 525000, ymx = 550000,
+               vals = 1)
+crs(rmask) <- crs(elev500)              
+          
+plot(rmask, add=TRUE)
+plot(xy1_pt, add=TRUE, pch=16)
+
+elev_sub <- crop(elev500, rmask)
+plot(elev_sub)
 plot(xy1_pt, add=TRUE, pch=16)
 
 
-
-
-
-xy2mat <- matrix(coordinates(elev500),
+xy2mat <- matrix(coordinates(elev_sub),
                  ncol=2,
-                 nrow=coordinates(elev500))
+                 nrow=nrow(coordinates(elev_sub)))
 
-viewTo.res <- viewTo(elev500, xy=as.vector(xy1_pt), xy2=xy2mat, h1=h1, h2=h2)
+viewTo.res <- viewTo(elev_sub, xy=as.vector(xy1_pt), xy2=xy2mat, h1=h1, h2=h2)
 seenby <- xy2mat[viewTo.res,]
 
+seenby_mpt <- st_multipoint(seenby)
+
+plot(elev_sub)
+plot(xy1_pt, add=TRUE, pch=16)
+plot(seenby_mpt, add=TRUE, cex=0.5)
+
+plot(elev_sub)
+plot(view500, add=TRUE)
