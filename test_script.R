4dddb5f230ac842bf89cc690df1838ad2e9a446a
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2021-02-07 14:40:43 +0000

    viewTo() function expects matrix of coordinates and now returns set of TRUE or FALSE values

--- test_script.R
+++ test_script.R
@@ -5,7 +5,7 @@ library(raster)
 # Elevation raster, 1 m resolution
 elev1 = raster(nrows = 6, ncols = 6, res = 1, 
                        xmn = 0, xmx = 6, ymn = 0, ymx = 6,
-                       vals = 1:36)
+                       vals = c(1:18,18:1))
 plot(elev1)
 
 
@@ -16,7 +16,7 @@ plot(elev1)
 # e.g. track along top row of map horizontally
 rasterprofile(elev1, c(0.5,5.5), c(5.5,5.5))
 
-# profile along southern most row of map east to west
+# profile along southern most row of map west to east
 rasterprofile(elev1, c(0.5,0.5), c(5.5,0.5))
 
 # profile along western most edge of map, south to north
@@ -29,8 +29,21 @@ rasterprofile(elev1, c(0.5,0.5), c(0.5,5.5))
 # Diagonally from north west to south east
 rasterprofile(elev1, c(0.5,5.5), c(5.5,0.5))
 
+# Diagonally from south west to north east
+rasterprofile(elev1, c(0.5,0.5), c(5.5,5.5))
+
+# Diagonally along highest point of map
+rasterprofile(elev1, c(0.5,2.5), c(5.5,3.5))
+
+
 # So what does cansee() return: TRUE or FALSE
-cansee(elev1, c(0.5,0.5), c(5.5,5.5), h1=2, h2=0)
+rasterprofile(elev1, c(0.5,0.5), c(0.5,4.5))
+cansee(elev1, c(0.5,0.5), c(0.5,4.5), h1=0, h2=23)
 
 # Does viewTo now work? No. Gives an error.
-viewTo(elev1, c(0.5,0.5), c(5.5,5.5), h1=2, h2=0)
+xy2mat <- matrix(coordinates(elev1),
+                 ncol=2,
+                 nrow=36,
+                 byrow = TRUE)
+xy2mat
+viewTo(elev1, c(0.5,0.5), xy2=xy2mat, h1=0, h2=23)
